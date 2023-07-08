86a15f89187c06e1b476c917d2d0d6c477b62b04
Bohdan <semeniuk.bm@gmail.com>
2023-07-09 01:47:35 +0300

    complete mobile version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+(function() {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.scrollY > 50) {
+            header.classList.add('header_active');
+        }
+        else {
+            header.classList.remove('header_active');
+        }
+    }
+}()); 
 (function () {
     const hamburgerMenu = document.querySelector('.header__burger');
     const menuCloseItem = document.querySelector('.header__nav-close');
